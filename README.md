ac0743578fd65b2484f7bf8467405bc24d0c3cfe
Filipe Rezende <fsr.trabalho@gmail.com>
2021-09-19 19:10:25 -0400

    project start

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # NodeJS-JWT
+Project Node.JS basic with JWT auth
+
+# Before start
+Adjust the .ENV file like sample
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/api/test", async (req, res) => {
+  console.log("test is successfull!");
+  res.status(200).json("test is successfull!");
+});
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
